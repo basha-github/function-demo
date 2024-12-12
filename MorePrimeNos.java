894ea84b818a0a1fd25276894215dc92cbdc116a
sharifabee <sharifabee.alur@gmail.com>
2024-12-12 09:40:30 +0530

    prime no 3

--- MorePrimeNos.java
+++ MorePrimeNos.java
@@ -4,29 +4,48 @@ public class MorePrimeNos {
 
 	public static void main(String[] args) {
 
-		
-		
-		// b.... e
-		//
 		int start=0,end=0;
+		char ch = ' ';
 		Scanner sc = new Scanner(System.in);
-		System.out.println("enter range");
-		start = sc.nextInt();
-		end = sc.nextInt();
-		int p=0;
-		int index=0;
-		for(int i=start;i<=end;i++) {
-			if(isPrime(i) == true) {
-				//System.out.print(i+" ");
-				p=i;
-				index++;
-				if(index == 13) {
-					break;
+		
+		do {
+			
+			System.out.println("enter range");
+			start = sc.nextInt();
+			end = sc.nextInt();
+			for(int i=start;i<=end;i++) {
+				if(isPrime(i) == true) {
+					
+					//System.out.print(i);
+					if(checkNoHav3(i)== true) {
+						System.out.print(i+" ");
+					}
 				}
+			}
+		
+			
+			System.out.println("\ndo u want to repeat");
+			ch = sc.next().charAt(0);
+			//if(ch == 'n') break;
+		}while(ch != 'n');
+		
+		
+		
+			
+		//System.out.println(p);
+	}
+	private static boolean checkNoHav3(int num) {
+	
+		// 107
+		for(int r;num != 0;) {
+			r = num % 10;
+			num = num /10;
+			if(r == 3) {
+				return true;
 			}
 		}
 		
-		System.out.println(p);
+		return false;
 	}
 	private static boolean isPrime(int n) {
 		int count=0;
